fc572c89dfbe5ed9d7448d5e925e268bfa707f80
Guillermo Caceres <guillermocaceresramos@gmail.com>
2024-02-07 10:41:01 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/ReadRift/ReadRiftApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ReadRift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadRiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadRiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/ReadRift/ReadRiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ReadRift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadRiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
